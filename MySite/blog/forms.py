e68330acad28974a076bffb677ff971225a4d22c
Khanh Hoang <thoang43@montgomerycollege.edu>
2019-07-15 22:34:02 -0400

    Finish Model, Form. Next: Create templates, views, then connect urls

---
+++ MySite/blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from blog.models import Post, Comment
+
+class PostForm(forms.ModelForm):
+    class Meta():
+        model = Post
+        fields = ('author','title','text')
+        widgets={
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.TextArea(attrs={'class':'editble medium-editor-textarea postcontent'})
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta():
+        model = Comment
+        fields = ('author','text')
+        widgets={
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.TextArea(attrs={'class':'editble medium-editor-textarea'})
+        }
