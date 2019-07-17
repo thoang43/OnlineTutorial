d9dc7cc7e704ad37594cc2adfb86998d444ee1db
Khanh Hoang <thoang43@montgomerycollege.edu>
2019-07-16 21:12:47 -0400

    Finish Function-Based Views part

--- MySite/blog/views.py
+++ MySite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView, CreateView,
@@ -42,3 +43,42 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(publish_date__isnull = True).order_by('created_date')
+
+############################################
+############################################
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
+
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk)
+    post.publish()
+    return redirect('post_detail',pk = pk)
