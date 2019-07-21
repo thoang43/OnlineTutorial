a94bad0e941a7b33e13bf87b46f6f921383c55ee
Khanh Hoang <thoang43@montgomerycollege.edu>
2019-07-21 01:08:05 -0400

    So Sleepy. Partly done with models in posts and groups. Deal with views/forms tomorrow

--- SimpleSocial/groups/views.py
+++ SimpleSocial/groups/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.urls import reverse
+from django.views import generic
 # Create your views here.
+from groups.models import Group, GroupMember
+
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields=('name','description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
