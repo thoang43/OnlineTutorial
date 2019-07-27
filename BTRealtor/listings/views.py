9a773aa79ec0745f7289fcdda8a4358eb2edfec6
Khanh Hoang <thoang43@montgomerycollege.edu>
2019-07-27 18:20:03 -0400

    Change views from static info to database- pulled info

--- BTRealtor/listings/views.py
+++ BTRealtor/listings/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import bedroom_choices,price_choices,state_choices
 # Create your views here.
 def index(request):
 
-    listings = Listing.objects.all()
+    listings = Listing.objects.all().order_by('-list_date').filter(is_published=True)
 
-    context = {'listings':listings}
+    paginator = Paginator(listings,6)
+
+    page = request.GET.get('page')
+
+    paged_listings = paginator.get_page(page)
+
+    context = {'listings':paged_listings}
 
     return render(request,'listings/listings.html',context)
 
 def listing(request, listing_id):
-    return render(request,'listings/listing.html')
+    listing = get_object_or_404(Listing,pk=listing_id)
+    context={
+    'listing':listing
+    }
+    return render(request,'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    context = [
+    'listings':listings,
+    'state_choices':state_choices,
+    'price_choices':price_choices,
+    'bedroom_choices':bedroom_choices
+    ]
+    return render(request,'listings/search.html',context)
